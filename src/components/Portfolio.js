51370f1015631fe2335083ada5cf80ead6fa6ed3
faldhighifari <faldhighifari248@gmail.com>
2022-09-12 14:58:17 +0700

    second commit

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -24,28 +24,28 @@ export default function Portfolio() {
             <div class="overflow-hidden rounded-md shadow-md">
               <img src={LP} alt="Landing Page" width="w-full" />
             </div>
-            <h3 class="mt-5 mb-3 text-xl font-semibold text-dark dark:text-white">Landing Page Faldhi Ghifari</h3>
+            <h3 class="mt-5 mb-3 text-xl font-semibold text-dark dark:text-white"><a href="https://heartfelt-starship-441219.netlify.app/">Landing Page Faldhi Ghifari</a></h3>
             <p class="text-base font-medium text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, iusto! Aliquam, corporis.</p>
           </div>
           <div class="mb-12 p-4 md:w-1/2">
             <div class="overflow-hidden rounded-md shadow-md">
               <img src={EC} alt="E-Commerce" width="w-full" />
             </div>
-            <h3 class="mt-5 mb-3 text-xl font-semibold text-dark dark:text-white">E-Commerce</h3>
+            <h3 class="mt-5 mb-3 text-xl font-semibold text-dark dark:text-white"><a href="https://stalwart-frangollo-f70c84.netlify.app/">E-Commerce</a></h3>
             <p class="text-base font-medium text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, iusto! Aliquam, corporis.</p>
           </div>
           <div class="mb-12 p-4 md:w-1/2">
             <div class="overflow-hidden rounded-md shadow-md">
               <img src={NP} alt="Technical Documentation" width="w-full" />
             </div>
-            <h3 class="mt-5 mb-3 text-xl font-semibold text-dark dark:text-white">News Portal with Laravel</h3>
+            <h3 class="mt-5 mb-3 text-xl font-semibold text-dark dark:text-white"><a href="http://portalberita4.herokuapp.com/">News Portal with Laravel</a></h3>
             <p class="text-base font-medium text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, iusto! Aliquam, corporis.</p>
           </div>
           <div class="mb-12 p-4 md:w-1/2">
             <div class="overflow-hidden rounded-md shadow-md">
               <img src={LG} alt="Tribute Page" width="w-full" />
             </div>
-            <h3 class="mt-5 mb-3 text-xl font-semibold text-dark dark:text-white">Cloning LG Mobile page</h3>
+            <h3 class="mt-5 mb-3 text-xl font-semibold text-dark dark:text-white"><a href="https://silver-sfogliatella-1a45f3.netlify.app/">Cloning LG Mobile page</a></h3>
             <p class="text-base font-medium text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, iusto! Aliquam, corporis.</p>
           </div>
         </div>
